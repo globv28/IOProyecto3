6e4edb73379369511c0dcf508f793b3486016c61
globv28 <glori2803@gmail.com>
2023-09-17 13:56:29 -0600

    Toma valores de botones
    cantidad de juegos
    probabilidad de ganar en casa (a)
    probabilidad de ganar de visita (a)
    de botones

--- seriesDeportivas.c
+++ seriesDeportivas.c
@@ -1,3 +1,5 @@
+//gcc -o prueba  seriesDeportivas.c -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic -rdynamic
+//Ecuación de Bellman: Tabla[i][j] = p*tabla[i-1][j]* q*tabla[i][i-j]
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
 #include <stdio.h>
@@ -8,6 +10,52 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+//variables de gtk
+GtkWidget* window;
+
+GtkWidget* cantJuegos;
+GtkWidget* GanarCasa;
+GtkWidget* GanarVisita;
+
+GtkGrid* gameLocation;
+GtkGrid* probabilidades;
+
+//variables para manejar la vara
+int games;
+float probGanarCasa;
+float probPerderCasa;
+
+float probGanarVisita;
+float probPerderVisita;
+
+
+//matrices de valores and shit
+gboolean button_clicked = FALSE;
+
+const float *tablaProbabilidades[51][51]; //tabla en la que se van a trabajar los resultados de probabilidades
+
+const int juegos[] = {
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25,
+    26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+    36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+    46, 47, 48, 49, 50, 51
+};
+
+const int juegoEnCasa[] = { //0 = falso, no es juego en casa de a, 1 = verdadero, juegan en casa de A
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0
+};
+
+
+void exit_app(){
+    gtk_main_quit();
+}
 
 static void load_css(void){
     GtkCssProvider *provider;
@@ -30,23 +78,94 @@ static void load_css(void){
 }
 
 
+void cleanTable1(){
+	for (int i = 0; i < games + 1; i++) { //limpia la tabla que se va a ctualizando
+		gtk_grid_remove_row(gameLocation, 0);
+    }
+}
+void cleanTable2(){ //limpia la tabla del display de la respuesta final
+	for (int i = 0; i < games + 2; i++) {
+		gtk_grid_remove_row(probabilidades, 0);
+    }
+}
+
+gboolean actualizarCantJuego( GtkWidget *widget, GdkEventButton *event, gpointer object )
+{
+    button_clicked = TRUE;
+	/*cleanTable1();
+    cleanTable2();*/
+    games = gtk_spin_button_get_value_as_int((GtkSpinButton*) cantJuegos);
+    g_print("cantJuegos: %d\n", games);
+	/*createTable1();
+	createTable2();*/
+    gtk_widget_show_all(window);
+
+    return FALSE;
+}
+
+gboolean actualizarGanarCasa (GtkWidget *widget, GdkEventButton *event, gpointer object )
+{
+    button_clicked = TRUE;
+    probGanarCasa = gtk_spin_button_get_value((GtkSpinButton*) GanarCasa);
+    probPerderCasa = 1 - probGanarCasa;
+    
+    // Imprime los valores en pantalla
+    g_print("probGanarVisita: %f\n", probGanarCasa);
+    g_print("probPerderVisita: %f\n", probPerderCasa);
+    
+    gtk_widget_show_all(window);
+
+    return FALSE;
+}
+
+gboolean actualizarGanarVisita (GtkWidget *widget, GdkEventButton *event, gpointer object )
+{
+    button_clicked = TRUE;
+    probGanarVisita = gtk_spin_button_get_value((GtkSpinButton*) GanarVisita);
+    probPerderVisita = 1 - probGanarVisita;
+
+    // Imprime los valores en pantalla
+    g_print("probGanarVisita: %f\n", probGanarVisita);
+    g_print("probPerderVisita: %f\n", probPerderVisita);
+
+    gtk_widget_show_all(window);
+
+    return FALSE;
+}
+
+
 int main(int argc, char *argv[]){
     GtkBuilder *builder; //GTK builder
     gtk_init(&argc, &argv); //start gtk
 
     builder = gtk_builder_new(); //create gtk ui builder
-    gtk_builder_add_from_file(builder, "mochila.glade", NULL); //LOAD UI FILE
+    gtk_builder_add_from_file(builder, "seriesDeportivas.glade", NULL); //LOAD UI FILE
     
     load_css();
-    
+    //ventana
     window = GTK_WIDGET(gtk_builder_get_object(builder, "MyWindow")); //load window named MyWindow
 
+    //botones
+    cantJuegos = GTK_WIDGET(gtk_builder_get_object(builder, "cant_juegos")); 
+	GanarCasa = GTK_WIDGET(gtk_builder_get_object(builder, "home"));
+    GanarVisita = GTK_WIDGET(gtk_builder_get_object(builder, "visit"));
+
+    //tablas
+    gameLocation = GTK_GRID(gtk_builder_get_object(builder, "games_location"));
+    probabilidades = GTK_GRID(gtk_builder_get_object(builder, "games_location"));
     
     //ASIGN VARIABLES
+    games = 3;
+    probGanarCasa = 0;
+    probGanarVisita = 0;
+
+    //connect signals
+    g_signal_connect( cantJuegos, "activate", G_CALLBACK(actualizarCantJuego), NULL );
+    g_signal_connect( GanarCasa, "activate", G_CALLBACK(actualizarGanarCasa), NULL );
+    g_signal_connect( GanarVisita, "activate", G_CALLBACK(actualizarGanarVisita), NULL );
 
     gtk_builder_connect_signals(builder, NULL);
     
-    FileChooserWindow = GTK_WIDGET(gtk_builder_get_object(builder, "FileChooserWindow"));
 
     g_object_unref(builder);
 
